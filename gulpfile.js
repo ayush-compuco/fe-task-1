784e6be902c95989d9b32ff657294bfef0381429
Ayush Mishra <ayush@compuco.io>
2022-09-14 21:58:27 +0530

    Initial commit with html structure

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglifycss = require('gulp-uglifycss');
+ 
+gulp.task('sass', function(){
+    return gulp.src('./styles/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(uglifycss({
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('./styles/css/'));
+});
+
+gulp.task('run', gulp.series('sass'));
+
+gulp.task('watch', function (){
+    gulp.watch('./styles/sass/*.scss', gulp.series('sass'));
+});
+gulp.task('default', gulp.series('run', 'watch'));
